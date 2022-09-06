b5fd240d619ea5a4923b1caf0f9293719e760886
Somala Prasanna Kumar <104211409+Prasannakumar55@users.noreply.github.com>
2022-09-06 13:32:29 +0530

    Update day1.jsx

--- day1.jsx
+++ day1.jsx
@@ -25,7 +25,9 @@
 
     functional Component :
 
-    A functional component is just a plain JavaScript pure function that accepts props as an argument and returns a React element. There is no render method used in functional components. Hooks can be easily used in functional components to make them Stateful.
+    A functional component is just a plain JavaScript pure function that accepts props
+    as an argument and returns a React element. There is no render method used in 
+    functional components. Hooks can be easily used in functional components to make them Stateful.
 
         Example :
 
@@ -43,7 +45,9 @@
 
     Class Component :   
     
-    A class component requires you to extend from React. Component and create a render function which returns a React element. It must have the render() method returning JSX. React lifecycle methods can be used inside class components (for example, componentDidMount).
+    A class component requires you to extend from React. Component and create a render function 
+    which returns a React element. It must have the render() method returning JSX. 
+    React lifecycle methods can be used inside class components (for example, componentDidMount).
 
         Example :
 
